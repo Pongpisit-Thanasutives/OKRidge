ba8a7d08e6208924f0387407ae6f8e08c70e7e02
Pongpisit Thanasutives <pongpisitthanasutives@Pongpisits-MacBook-Air.local>
2024-04-10 14:30:46 +0900

    fix

--- src/okridge/solvel0.py
+++ src/okridge/solvel0.py
@@ -2,12 +2,12 @@ import numpy as np
 from tqdm import trange
 from .tree import BNBTree
 
-def okridge_solvel0(X, y, k, lambda2=1e-5, gap_tol=1e-5, useBruteForce=True, time_limit=180, verbose=False):
+def okridge_solvel0(X, y, k, lambda2=1e-5, gap_tol=1e-4, useBruteForce=True, time_limit=180, verbose=False):
     BnB_optimizer = BNBTree(X=X, y=y.flatten(), lambda2=lambda2, useBruteForce=useBruteForce, verbose=verbose)
     _, beta, _, _, _ = BnB_optimizer.solve(k=int(k), gap_tol=gap_tol, verbose=verbose, time_limit=time_limit)
     return beta, np.nonzero(beta)[0].tolist()
 
-def okridge_solvel0_full(X, y, k, lambda2=1e-5, gap_tol=1e-5, useBruteForce=True, time_limit=180, verbose=False):
+def okridge_solvel0_full(X, y, k, lambda2=1e-5, gap_tol=1e-4, useBruteForce=True, time_limit=180, verbose=False):
     BnB_optimizer = BNBTree(X=X, y=y.flatten(), lambda2=lambda2, useBruteForce=useBruteForce, verbose=verbose)
     betas = np.zeros((k, X.shape[-1]))
     nonzero_indices = []
